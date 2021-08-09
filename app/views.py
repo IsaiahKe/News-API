78ec37fd1014facd9d7e9ece032f4d5f0d0a88ce
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-09 23:35:36 +0300

    add confoguration source and news category functions

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from datetime import date
-from app.request import get_article, get_by_source
+from app.request import get_article, get_by_category, get_by_source
 from flask import render_template
 from app import app
 
@@ -8,8 +8,9 @@ def index():
 
     message="isaiah morara"
     timeframe= date.today()
-    me=get_article('biden')
-    return render_template('index.html',message=message,me=me)
+    me=get_article()
+    sources=get_by_source()
+    return render_template('index.html',message=message,me=me,sources=sources)
 
 @app.route('/source')
 def source():
@@ -18,3 +19,9 @@ def source():
     '''
     us=get_by_source()
     return render_template('sources.html',us=us)
+
+@app.route('/category/<type>')
+def category(type):
+    category=get_by_category('us',type)
+    heading=type
+    return render_template('category.html',category=category,heading=heading)
